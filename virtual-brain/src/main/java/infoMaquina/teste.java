8920a5a8cdf4029f2f9046f1e0714a938c4cc4a9
paulacapuano <paula.macedo@bandtec.com.br>
2020-11-29 19:39:39 -0300

    teste banco marise corrigindo4

--- virtual-brain/src/main/java/infoMaquina/teste.java
+++ virtual-brain/src/main/java/infoMaquina/teste.java
@@ -15,7 +15,7 @@ public class teste {
         Processos processos = new Processos();
         Cpu cpu = new Cpu();
         Leituras lt = new Leituras();
-       
+
         System.out.println(sistema.getSistemaOperacional());
         System.out.println("------------------------");
         System.out.println(processador.processador.subSequence(0, processador.indiceProcessador));
@@ -38,14 +38,17 @@ public class teste {
 //            Thread.sleep(5000);
 //        } catch (Exception e) {
 //        }}
-        
+
 //        String hostname = "";
 //        hostname = lt.getHostname();
-        
-        if (lt.getHostname().equals("6811b44a5f14")){
+        if (lt.getHostname().equals("6811b44a5f14")) {
             conexao.ConexaoMarise config = new conexao.ConexaoMarise();
             JdbcTemplate con = new JdbcTemplate(config.getDatasource());
-           con.update("INSERT INTO leituras VALUES (null, ? , NOW(), 1)", String.valueOf(cpu.usoCpu()));
+            for (int i = 0; i < 10; i++) {
+                con.update("INSERT INTO leituras VALUES (null, ? , NOW(), 1)", String.valueOf(cpu.usoCpu()));
+                con.update("INSERT INTO leituras VALUES (null, ? , NOW(), 2)", String.valueOf(mem.usoMemoria()));
+                
+            }
         }
     }
 }
